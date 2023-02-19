6f379952ce76bb83c4fdcca1d0d49e6d0baa2b9b
Simon Blom <102825244+simonblo@users.noreply.github.com>
2023-02-19 17:51:08 +0100

    Issue #12: Extends the testing of `FlashSort.flashSort`
    Extends the testing with a case where the input array consists of multiple elements that are all of the same value.

--- Sorts/test/FlashSort.test.js
+++ Sorts/test/FlashSort.test.js
@@ -23,3 +23,9 @@ test('The flash sort of the array [-3, 0, -2, -5, -1, -4, -1, -2] is [-5, -4, -3
   const res = flashSort(array)
   expect(res).toEqual([-5, -4, -3, -2, -2, -1, -1, 0])
 })
+
+test('The flash sort of the array [4, 4, 4, 4, 4] is [4, 4, 4, 4, 4]', () => {
+  const array = [4, 4, 4, 4, 4]
+  const res = flashSort(array)
+  expect(res).toEqual([4, 4, 4, 4, 4])
+})
