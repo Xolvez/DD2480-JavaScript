ce0414e95ff2d38a01357c273e5e999a4edb0ae2
Simon Blom <102825244+simonblo@users.noreply.github.com>
2023-02-19 15:27:55 +0100

    Issue #10: Implements an ad-hoc coverage tool
    Implements an ad-hoc coverage tool that measures the testing coverage of all possible branches in the function `FlashSort.flashSort`.

--- Sorts/FlashSort.js
+++ Sorts/FlashSort.js
@@ -6,38 +6,57 @@
  * Wikipedia: https://en.wikipedia.org/wiki/Flashsort
  */
 
-export function flashSort (arr) {
+export function flashSort (arr, __COVERAGE__) {
   let max = 0; let min = arr[0]
   const n = arr.length
   const m = ~~(0.45 * n)
   const l = new Array(m)
 
   for (let i = 1; i < n; ++i) {
+    __COVERAGE__[0] = true
     if (arr[i] < min) {
+      __COVERAGE__[1] = true
       min = arr[i]
+    } else {
+      __COVERAGE__[2] = true
     }
     if (arr[i] > arr[max]) {
+      __COVERAGE__[3] = true
       max = i
+    } else {
+      __COVERAGE__[4] = true
     }
+  } if (!(1 < n)) {
+    __COVERAGE__[5] = true
   }
 
   if (min === arr[max]) {
+    __COVERAGE__[6] = true
     return arr
+  } else {
+    __COVERAGE__[7] = true
   }
 
   const c1 = (m - 1) / (arr[max] - min)
 
   for (let k = 0; k < m; k++) {
+    __COVERAGE__[8] = true
     l[k] = 0
+  } if (!(0 < m)) {
+    __COVERAGE__[9] = true
   }
 
   for (let j = 0; j < n; ++j) {
+    __COVERAGE__[10] = true
     const k = ~~(c1 * (arr[j] - min))
     ++l[k]
   }
 
   for (let p = 1; p < m; ++p) {
+    __COVERAGE__[11] = true
     l[p] = l[p] + l[p - 1]
+  } if (!(1 < m)) {
+    __COVERAGE__[12] = true
   }
 
   let hold = arr[max]
@@ -50,27 +69,43 @@ export function flashSort (arr) {
   let k = m - 1
 
   while (move < (n - 1)) {
+    __COVERAGE__[13] = true
     while (j > (l[k] - 1)) {
+      __COVERAGE__[14] = true
       ++j
       k = ~~(c1 * (arr[j] - min))
+    } if (!(j > (l[k] - 1))) {
+      __COVERAGE__[15] = true
     }
-    if (k < 0) break
+    if (k < 0) {
+      __COVERAGE__[16] = true
+      break
+    } else {
+      __COVERAGE__[17] = true
+    }
     flash = arr[j]
     while (j !== l[k]) {
+      __COVERAGE__[18] = true
       k = ~~(c1 * (flash - min))
       hold = arr[t = --l[k]]
       arr[t] = flash
       flash = hold
       ++move
+    } if (!(j !== l[k])) {
+      __COVERAGE__[19] = true
     }
   }
 
   // insertion
   for (j = 1; j < n; j++) {
+    __COVERAGE__[20] = true
     hold = arr[j]
     let i = j - 1
     while (i >= 0 && arr[i] > hold) {
+      __COVERAGE__[21] = true
       arr[i + 1] = arr[i--]
+    } if (!(i >= 0 && arr[i] > hold)) {
+      __COVERAGE__[22] = true
     }
     arr[i + 1] = hold
   }
--- Sorts/test/FlashSort.test.js
+++ Sorts/test/FlashSort.test.js
@@ -1,25 +1,37 @@
 import { flashSort } from '../FlashSort'
 
+var __COVERAGE__ = new Array(23).fill(false)
+
+afterAll(() => {
+  let message = ""
+  for (let i = 0; i < 23; ++i) {
+    if (!__COVERAGE__[i]) {
+      message += "Branch " + i + " was not covered.\n"
+    }
+  }
+  console.log(message)
+})
+
 test('The flash sort of the array [3, 0, 2, 5, -1, 4, 1, -2] is [-2, -1, 0, 1, 2, 3, 4, 5]', () => {
   const array = [3, 0, 2, 5, -1, 4, 1, -2]
-  const res = flashSort(array)
+  const res = flashSort(array, __COVERAGE__)
   expect(res).toEqual([-2, -1, 0, 1, 2, 3, 4, 5])
 })
 
 test('The flash sort of the array [-3, 0, 2, -5, -1, 4, 1, -2] is [-5, -3, -2, -1, 0, 1, 2, 4]', () => {
   const array = [-3, 0, 2, -5, -1, 4, 1, -2]
-  const res = flashSort(array)
+  const res = flashSort(array, __COVERAGE__)
   expect(res).toEqual([-5, -3, -2, -1, 0, 1, 2, 4])
 })
 
 test('The flash sort of the array [13, 0, 12, 5, -1, 14, 1, -2] is [-2, -1, 0, 1, 5, 12, 13, 14]', () => {
   const array = [13, 0, 12, 5, -1, 14, 1, -2]
-  const res = flashSort(array)
+  const res = flashSort(array, __COVERAGE__)
   expect(res).toEqual([-2, -1, 0, 1, 5, 12, 13, 14])
 })
 
 test('The flash sort of the array [-3, 0, -2, -5, -1, -4, -1, -2] is [-5, -4, -3, -2, -2, -1, -1, 0]', () => {
   const array = [-3, 0, -2, -5, -1, -4, -1, -2]
-  const res = flashSort(array)
+  const res = flashSort(array, __COVERAGE__)
   expect(res).toEqual([-5, -4, -3, -2, -2, -1, -1, 0])
 })
