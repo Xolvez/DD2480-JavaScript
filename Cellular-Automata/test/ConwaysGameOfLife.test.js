9442562c0a86a4b5bd013e2a1b771faad35f41d3
Pauline Garelli <pauline.garelli@hotmail.fr>
2023-02-20 16:14:54 +0100

    Issue #53: Adds branch coverage computation in test for newGeneration
    In the test suite of ConwaysGameOfLife,
    we compute the branch coverage and display it in logs.

--- Cellular-Automata/test/ConwaysGameOfLife.test.js
+++ Cellular-Automata/test/ConwaysGameOfLife.test.js
@@ -1,8 +1,24 @@
 import { newGeneration } from '../ConwaysGameOfLife'
 
+var __COVERAGE__ = new Array(22).fill(false)
+
+afterAll(() => {
+  let message = ""
+  for (let i = 0; i < 22; ++i) {
+    if (!__COVERAGE__[i]) {
+      message += "Branch " + i + " was not covered.\n"
+    }
+  }
+  var branches_covered = __COVERAGE__.filter(Boolean).length
+  var branch_coverage = branches_covered / 22 * 100
+  message += "Total branch coverage: " + branches_covered + " out of 22, ie " + branch_coverage + " %"
+  console.log(message)
+})
+
 describe('newGeneration', () => {
   it('should produce the next generation according to the rules', () => {
-    expect(newGeneration([[0, 1, 0], [0, 1, 0], [0, 1, 0]]))
+    const res = newGeneration([[0, 1, 0], [0, 1, 0], [0, 1, 0]], __COVERAGE__)
+    expect(res)
       .toEqual([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
   })
 })
