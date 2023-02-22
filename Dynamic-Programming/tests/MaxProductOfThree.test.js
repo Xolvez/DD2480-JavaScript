4301dbf6766528b4308f9ac91fe913e52cf49e7e
oscarknowles <oscar@knowles.se>
2023-02-22 14:06:54 +0100

    Issue86 FEAT: Implementation of ad-hoc coverage tool
    Coverage tool implemented for function maxProductOfThree

--- Dynamic-Programming/tests/MaxProductOfThree.test.js
+++ Dynamic-Programming/tests/MaxProductOfThree.test.js
@@ -1,17 +1,29 @@
 import { maxProductOfThree } from '../MaxProductOfThree'
 
+var __COVERAGE__ = new Array(9).fill(false)
+
+ afterAll(() => {
+   let message = ""
+   for (let i = 0; i < 9; ++i) {
+     if (!__COVERAGE__[i]) {
+       message += "Branch " + i + " was not covered.\n"
+     }
+   }
+   console.log(message)
+ })
+
 describe('MaxProductOfThree', () => {
   it('expects to throw error for array with only 2 numbers', () => {
     expect(() => {
-      maxProductOfThree([1, 3])
+      maxProductOfThree([1, 3], __COVERAGE__)
     }).toThrow('Triplet cannot exist with the given array')
   })
 
   it('expects to return 300 as the maximum product', () => {
-    expect(maxProductOfThree([10, 6, 5, 3, 1, -10])).toBe(300)
+    expect(maxProductOfThree([10, 6, 5, 3, 1, -10], __COVERAGE__)).toBe(300)
   })
 
   it('expects to return 300 as the maximum product', () => {
-    expect(maxProductOfThree([10, -6, 5, 3, 1, -10])).toBe(600)
+    expect(maxProductOfThree([10, -6, 5, 3, 1, -10], __COVERAGE__)).toBe(600)
   })
 })
